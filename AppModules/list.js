88c73542708625de54f9017bf5eb3e675e33d0b6
Adolfo92 <adolfo92dev@gmail.com>
2023-10-02 11:54:53 -0400

    Various changes
    Yes, it was important to make the most amount of commits before, but i totally forgot about it.
    
    the previously named test.test.js was renamed list.test.js, therefore, the previous index.js was renamed list.js and moved into a folder named AppModules
    
    The ney file ui.js was also created inde AppModules with it's respective test file
    
    On the subject of the previous code:
    
    *A bunch of test were added to list.test.js wich i guess is not the best way of developing something, but i just learned to make tests and wanted to use them as a kind of documentation.
    
    *list.js contains 2 classes, the list class in charge of creating list objects with methods to add, erase, and read the content.

---
+++ AppModules/list.js
@@ -0,0 +1,62 @@
+/* eslint-disable quotes */
+class List {
+  choreList = [];
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getList() {
+    return this.choreList;
+  }
+
+  addChore(chore) {
+    if (typeof chore !== "string") {
+      throw new Error(
+        "El metodo addChore() de la clase List debe recibir un numero"
+      );
+    }
+    const lowercaseChore = chore.toLowerCase();
+    this.choreList.push(lowercaseChore);
+  }
+
+  removeChore(listIndex) {
+    if (typeof listIndex !== "number") {
+      throw new Error(
+        "El metodo removeChore() de la clase List debe recibir un numero"
+      );
+    }
+    const arrayIndex = listIndex - 1;
+    this.choreList.splice(arrayIndex, 1);
+  }
+
+  addObjectChore(chore) {
+    const newChore = new Chore(chore);
+    this.choreList.push(newChore);
+  }
+}
+
+class Chore {
+  done = false;
+  constructor(name) {
+    this.name = name;
+  }
+
+  changeStatus() {
+    this.done === true ? (this.done = false) : (this.done = true);
+  }
+  setDescription(text) {
+    this.description = text;
+  }
+  setDueDate(date) {
+    this.dueDate = date;
+  }
+  setPriority(urgency) {
+    this.priority = urgency;
+  }
+}
+
+export { List };
---
+++ test/list.test.js
@@ -0,0 +1,74 @@
+import { List, toDoList } from "../AppModules/list";
+describe("***List", () => {
+  test("Create one list", () => {
+    const expected = "Tareas de casa";
+    expect(new List("Tareas de casa").getName()).toBe(expected);
+  });
+});
+
+describe("***List chore adding", () => {
+  const newList = new List("random name");
+
+  test("Add one Chore", () => {
+    newList.addChore("test1");
+    const expected = ["test1"];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Add two more chores", () => {
+    newList.addChore("test2");
+    newList.addChore("test3");
+    const expected = ["test1", "test2", "test3"];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Add mixed uppercase chores", () => {
+    newList.addChore("teST4");
+    const expected = ["test1", "test2", "test3", "test4"];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Error if add chore is passed other than a string", () => {
+    expect(() => {
+      newList.addChore(1254);
+    }).toThrow(/addChore/);
+  });
+
+  test("Remove chore by list index", () => {
+    newList.removeChore(3);
+    const expected = ["test1", "test2", "test4"];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Error if remove chore is passed other than a number", () => {
+    expect(() => {
+      newList.removeChore("abcdef");
+    }).toThrow(/removeChore/);
+  });
+});
+
+describe("***Object Chore", () => {
+  const newList = new List("randomName");
+
+  test("add Object Chore", () => {
+    newList.addObjectChore("test");
+    const expected = [{ name: "test", done: false }];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Change Object chore status", () => {
+    const item1 = newList.getList()[0];
+    item1.changeStatus();
+    const expected = [{ name: "test", done: true }];
+    expect(newList.getList()).toEqual(expected);
+  });
+
+  test("Add property to object Chore", () => {
+    const item1 = newList.getList()[0];
+    item1.setDescription("Some random text");
+    const expected = [
+      { name: "test", done: true, description: "Some random text" },
+    ];
+    expect(newList.getList()).toEqual(expected);
+  });
+});
