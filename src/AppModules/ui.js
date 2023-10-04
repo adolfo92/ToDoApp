94cc175794e751f4c39e622954afa22bf751745e
Adolfo92 <adolfo92dev@gmail.com>
2023-10-03 20:14:21 -0400

    Add a function that creates a container to display the chores properties

--- src/AppModules/ui.js
+++ src/AppModules/ui.js
@@ -7,4 +7,32 @@ function createListUIon(parent) {
 
   parent.appendChild(listDiv);
 }
-export { createListUIon };
+
+function _makeChoreContainer() {
+  const choreContainer = document.createElement("div");
+  const deleteButton = document.createElement("button");
+
+  choreContainer.classList.add("choreContainer");
+  deleteButton.classList.add("delete");
+
+  choreContainer.appendChild(deleteButton);
+
+  return choreContainer;
+}
+
+function displayChoreUI(choreObj) {
+  const choreContainer = _makeChoreContainer();
+
+  for (key in choreObj) {
+    const propertyContainer = document.createElement("div");
+    propertyContainer.classList.add("property");
+    propertyContainer.classList.add(`${key}`);
+    propertyContainer.textContent = `${choreObj[key]}`;
+
+    choreContainer.appendChild(propertyContainer);
+  }
+
+  return choreContainer;
+}
+
+export { createListUIon, displayChoreUI };
