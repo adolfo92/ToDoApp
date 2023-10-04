fac69a0e591b23e822a9a927a471b74df9893e65
Adolfo92 <adolfo92dev@gmail.com>
2023-10-04 14:27:11 -0400

    Add temporal function to paint the list on website

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import { List } from "./AppModules/list";
-import { createListUIon } from "./AppModules/ui";
+import { createListUIon, displayChoreUI } from "./AppModules/ui";
+
+//------- MockupList
+const myList = new List("mockup list");
+myList.addObjectChore("regar las matas");
+myList.addObjectChore("comprar cigarros");
+myList.addObjectChore("llamar a oriana");
+const currentList = myList.getList();
+//-------
 
 const tablon = document.getElementById("board");
+
+createListUIon(tablon);
+
+const listdiv = document.querySelector(".list");
+
+console.log(currentList.length);
+
+for (let i = 0; i < currentList.length; i++) {
+  listdiv.appendChild(displayChoreUI(currentList[i]));
+}
