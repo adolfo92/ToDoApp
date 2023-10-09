cf41dfa0cee733cbdbdd4c83642f489880483484
Adolfo92 <adolfo92dev@gmail.com>
2023-10-09 08:13:38 -0400

    Add list and chore maker functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { List } from "./AppModules/list";
 import { createListUIon, displayChoreUI } from "./AppModules/ui";
-import { replaceUnderScores } from "./AppModules/helpers";
+import { replaceUnderScores, replaceWhiteSpace } from "./AppModules/helpers";
 
 const tablon = document.getElementById("board");
 
@@ -24,7 +24,46 @@ for (let i = 0; i < currentList.length; i++) {
   listdiv.appendChild(displayChoreUI(currentList[i], i));
 }
 
+const testList = listMaker("test");
+
+choreMaker(testList, "comprar ajo");
+choreMaker(testList, "barrer piso");
+
 function listMaker(listName, container = tablon) {
-  const myList = new List(listName);
+  const myListObj = new List(listName);
   createListUIon(container, listName);
+  const listID = replaceWhiteSpace(listName);
+  const listdiv = document.querySelector(`#${listID}`);
+  const listHeader = document.createElement("h1");
+  listHeader.textContent = `${listName}`;
+  listdiv.appendChild(listHeader);
+
+  return { myListObj, listID, listdiv };
+}
+
+function choreMaker(list, chore) {
+  if (chore) list.myListObj.addChore(chore);
+
+  resetListUI(list);
+  const currentList = list.myListObj.getList();
+  for (let i = 0; i < currentList.length; i++) {
+    list.listdiv.appendChild(displayChoreUI(currentList[i], i));
+  }
+}
+
+function updateUI() {
+  //location.reload();
+  return;
+}
+
+// resetea la lista, procurar que los divs en la lista sean los ultimos contenedores
+function resetListUI(list) {
+  const listNode = list.listdiv;
+  while (listNode.hasChildNodes()) {
+    if (listNode.lastChild.localName === "div") {
+      listNode.removeChild(listNode.lastChild);
+    } else {
+      return;
+    }
+  }
 }
