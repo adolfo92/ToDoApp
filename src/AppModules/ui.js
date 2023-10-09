fee8452c9d4bd77a3752d170657cf50b24f5c838
Adolfo92 <adolfo92dev@gmail.com>
2023-10-09 04:42:22 -0400

    Reorganize code

--- src/AppModules/ui.js
+++ src/AppModules/ui.js
@@ -1,11 +1,31 @@
 function createListUIon(parent) {
   const listDiv = document.createElement("div");
   const addChoreButton = document.createElement("button");
+
   listDiv.classList.add("list");
   addChoreButton.classList.add("addChore");
   listDiv.appendChild(addChoreButton);
 
   parent.appendChild(listDiv);
+}
+
+function displayChoreUI(choreObj, index = 0) {
+  const choreContainer = _makeChoreContainer();
+
+  for (let key in choreObj) {
+    if (key === "done") {
+      choreContainer.dataset.done = `${choreObj[key]}`;
+      continue;
+    }
+
+    _setIndex(choreContainer, index);
+    const propertyContainer = document.createElement("div");
+    _addClasses(propertyContainer, "property", `${key}`);
+    propertyContainer.textContent = `${choreObj[key]}`;
+    choreContainer.appendChild(propertyContainer);
+  }
+
+  return choreContainer;
 }
 
 function _makeChoreContainer() {
@@ -18,44 +38,18 @@ function _makeChoreContainer() {
   choreContainer.appendChild(deleteButton);
 
   return choreContainer;
-  // Resumen: crea el div y el button
-  // Le agrega una clase a ambos
-  // mete el boton en el div
-  // retorna el div
 }
 
-function displayChoreUI(choreObj, index = 0) {
-  const choreContainer = _makeChoreContainer();
+// Create data-key
+function _setIndex(container, number) {
+  console.log(number);
+  container.dataset.choreIndex = `${number}`;
+}
 
-  for (let key in choreObj) {
-    if (key === "done") {
-      choreContainer.dataset.done = `${choreObj[key]}`;
-      continue;
-    }
-    setIndex(choreContainer, index);
-    const propertyContainer = document.createElement("div");
-    propertyContainer.classList.add("property");
-    propertyContainer.classList.add(`${key}`);
-    propertyContainer.textContent = `${choreObj[key]}`;
-
-    choreContainer.appendChild(propertyContainer);
+function _addClasses(container, ...classes) {
+  for (let i = 0; i < classes.length; i++) {
+    container.classList.add(classes[i]);
   }
-
-  function setIndex(container, number) {
-    console.log(number);
-    container.dataset.choreIndex = `${number}`;
-  }
-
-  return choreContainer;
-  // Resumen: recibe Objeto junto a un index
-  // Crea el contenedor
-  // agrega la data del index al contenedor
-  // Pasa por cada uno de las propiedades del objeto
-  //  Si es la propiedad es "done" se la asigna como data-key al contenedor
-  //  Si no es done, crea un div, le agrega dos clases, property y la propiedad
-  //  el contenido de la propiedad se agrega como texto dentro del div
-  //  Se mete el nuevo div dentro del contenedor
-  // Retorna el contenedor
 }
 
 export { createListUIon, displayChoreUI };
