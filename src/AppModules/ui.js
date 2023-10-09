aff4e14a8d5b9f3e662f591a375fde766f161311
Adolfo92 <adolfo92dev@gmail.com>
2023-10-09 13:02:00 -0400

    Add all the div creation and List object invocations from index.js, also make a list bundle hack into addChoreEvent function to work with createListUIon function so it wont break

--- src/AppModules/ui.js
+++ src/AppModules/ui.js
@@ -1,13 +1,63 @@
 import { _setIndex, _addClasses, replaceWhiteSpace } from "./helpers";
-function createListUIon(parent, listName) {
+import { List } from "./list";
+function listMaker(listName, container) {
+  const myListObj = new List(listName);
+  const listID = replaceWhiteSpace(listName);
+  createListUIon(container, myListObj);
+  const listdiv = document.querySelector(`#${listID}`);
+  const listHeader = document.createElement("h1");
+  listHeader.textContent = `${listName}`;
+  listdiv.appendChild(listHeader);
+  const bundle = { myListObj, listID, listdiv };
+
+  return { myListObj, listID, listdiv };
+}
+
+// Agrega tareas y/o actualiza la UI de la lista
+function choreMaker(list, chore) {
+  // Requiere la lista instanciada
+  // Se alimenta del objeto lista y del div lista
+  if (chore) list.myListObj.addChore(chore);
+  resetListUI(list.listdiv); // !!!!
+  const currentList = list.myListObj.getList();
+  for (let i = 0; i < currentList.length; i++) {
+    list.listdiv.appendChild(displayChoreUI(currentList[i], i));
+  }
+}
+
+// resetea la lista, procurar que los divs en la lista sean los ultimos contenedores
+function resetListUI(listDiv) {
+  // Requiere solamente el div de la lista
+  const listNode = listDiv;
+  while (listNode.hasChildNodes()) {
+    if (listNode.lastChild.localName === "div") {
+      listNode.removeChild(listNode.lastChild);
+    } else {
+      return;
+    }
+  }
+}
+
+function createListUIon(parent, listObject) {
   const listDiv = document.createElement("div");
   const addChoreButton = document.createElement("button");
+
   _addClasses(listDiv, "list");
-  listDiv.id = replaceWhiteSpace(listName);
+  listDiv.id = replaceWhiteSpace(listObject.getName());
+
   addChoreButton.classList.add("addChore");
+  addChoreEvent(addChoreButton, listObject, listDiv);
+
   listDiv.appendChild(addChoreButton);
 
   parent.appendChild(listDiv);
+}
+
+function addChoreEvent(buttonNode, listObject, listDiv) {
+  const listBundle = { myListObj: listObject, listdiv: listDiv };
+  buttonNode.addEventListener("click", (e) => {
+    choreMaker(listBundle, "chore test");
+  });
 }
 
 function displayChoreUI(choreObj, index = 0) {
@@ -41,4 +91,4 @@ function _makeChoreContainer() {
   return choreContainer;
 }
 
-export { createListUIon, displayChoreUI };
+export { createListUIon, displayChoreUI, listMaker, choreMaker };
