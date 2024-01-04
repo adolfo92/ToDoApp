03ee83610e87f32fc4262745aca390e0de0671e3
Adolfo92 <adolfo92dev@gmail.com>
2024-01-03 20:29:51 -0400

    Delete obsolete code

--- src/AppModules/objectUI.js
+++ src/AppModules/objectUI.js
@@ -10,6 +10,7 @@ class ListMaker {
   paintListOn(parent) {
     const listTitle = addTitleFrom(this.list.getName());
     this.container.appendChild(listTitle);
+    //this.container.appendChild(this.deleteList(parent));
     parent.appendChild(this.container);
   }
 
@@ -28,6 +29,21 @@ class ListMaker {
     removePreviousDataFrom(this.container);
     retieveThisChores();
   }
+  // Codigo comentado porque creo que mi problema tiene que ver con las
+  // referencias en memoria
+  /*deleteList(parent) {
+    const deleteListBtn = document.createElement("button");
+    deleteListBtn.classList.add("delete-list");
+    deleteListBtn.textContent = "Delete list";
+    deleteListBtn.addEventListener("click", () => {
+      console.log("Deleting:\n", this.list);
+      delete this.list;
+      removePreviousDataFrom(parent);
+      retrieveThisLists();
+      return;
+    });
+    return deleteListBtn;
+  }*/
 }
 
 function retrieveChores() {
@@ -39,6 +55,7 @@ function retrieveChores() {
     this.container.appendChild(choreContainer);
   }
 }
+
 function addDeleteButtonTo(container, index, context) {
   const button = document.createElement("button");
   button.classList.add("delteButton");
